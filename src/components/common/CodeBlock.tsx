53f18241e7448d088352ca42fd0727bd1b1b3fe7
Jorge Antonio Ramírez Padilla <jarp9665de@gmail.com>
2026-03-21 14:55:57 +0100

    fix: improve syntax highlighting dark mode

--- src/components/common/CodeBlock.tsx
+++ src/components/common/CodeBlock.tsx
@@ -19,8 +19,8 @@ export const CodeBlock = ({ code, language = 'javascript', className = '' }: Cod
   
   return (
     <div className={`bg-gray-100 dark:bg-gray-900 border border-gray-200 dark:border-gray-700 rounded-md p-3 mb-3 font-mono text-xs leading-relaxed overflow-x-auto ${className}`}>
-      <pre className="!bg-transparent !border-none !p-0 !m-0">
-        <code className={`language-${language} !bg-transparent text-xs leading-relaxed`}>
+      <pre className="!bg-transparent !border-none !p-0 !m-0 !font-mono">
+        <code className={`language-${language} !bg-transparent !font-mono text-xs leading-relaxed text-gray-900 dark:text-gray-100`}>
           {code}
         </code>
       </pre>
